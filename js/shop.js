2182775dc7c03915a610aec355767e9cc1744d32
peter <peter.lm.schmidt@gmail.com>
2021-03-15 16:00:21 +0100

    .

--- js/shop.js
+++ js/shop.js
@@ -10,4 +10,24 @@ const requestOption = {
 
 fetch(url, requestOption)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => gotData(data));
+
+function gotData(data){
+  console.log(data);
+  data.forEach(addRow(data));
+}
+
+function addRow(data) {
+  const table = document.getElementById("dataTable");
+  let rowCount = table.rows.length;
+  let row = table.insertRow(rowCount);
+
+  let cell1 = row.insertCell(0);
+  cell1.innerHTML = data.productId;
+
+  let cell2 = row.insertCell(1);
+  cell2.innerHTML = data.productName;
+
+  let cell3 = row.insertCell(2);
+  cell3.innerHTML = data.price;
+}
